fee1c6573024ecd029aeb9ed557a5f0fb3913c1a
euOnmyoji <1418780411@qq.com>
2020-02-23 12:37:56 +0800

    fix fatel err0R

--- src/main/java/com/github/euonmyoji/mcbbshelper/NewHelpGetter.java
+++ src/main/java/com/github/euonmyoji/mcbbshelper/NewHelpGetter.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,57 +26,59 @@ public class NewHelpGetter implements Runnable {
 
     @Override
     public void run() {
-        try {
-            final String urlPrefix = "https://www.mcbbs.net/";
-            URL url = new URL("https://www.mcbbs.net/forum.php?mod=guide&view=newthread&mobile=no");
-            LinkedList<String> list = new LinkedList<>();
-            while (Main.running) {
-                URLConnection con = url.openConnection();
-                boolean found = false;
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
-                    String[] lines = reader.lines().toArray(String[]::new);
-                    for (int i = 0; i < lines.length; ++i) {
-                        String s = lines[i];
-                        if (print) {
-                            System.out.println(i + ":" + s);
-                        }
-                        if (s.contains("target=\"_blank\" class=\"xst\" >")) {
-                            s = s.trim().replaceAll("(target=\"_blank\" class=\"xst\" >)|(<a href=)", "");
-                            Matcher matcher = PATTERN.matcher(s);
-                            if (matcher.matches()) {
-                                for (int j = 0; j < 6; ++j) {
-                                    if (lines[i + j].contains("悬赏") || lines[i + j].contains("金粒")) {
-                                        if (!list.contains(s)) {
-                                            if (!found) {
-                                                System.out.println();
-                                                found = true;
-                                            }
-                                            list.addLast(s);
-                                            while (list.size() > 70) {
-                                                list.removeFirst();
-                                            }
-                                            if (Main.state) {
-                                                try {
-                                                    Desktop.getDesktop().browse(new URI(urlPrefix + matcher.group("url")));
-                                                } catch (IOException | URISyntaxException e) {
-                                                    e.printStackTrace();
+        LinkedList<String> list = new LinkedList<>();
+        while(Main.running) {
+            try {
+                final String urlPrefix = "https://www.mcbbs.net/";
+                URL url = new URL("https://www.mcbbs.net/forum.php?mod=guide&view=newthread&mobile=no");
+                while (Main.running) {
+                    URLConnection con = url.openConnection();
+                    con.setReadTimeout(5000);
+                    con.setConnectTimeout(5000);
+                    boolean found = false;
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+                        String[] lines = reader.lines().toArray(String[]::new);
+                        for (int i = 0; i < lines.length; ++i) {
+                            String s = lines[i];
+                            if (print) {
+                                System.out.println(i + ":" + s);
+                            }
+                            if (s.contains("target=\"_blank\" class=\"xst\" >")) {
+                                s = s.trim().replaceAll("(target=\"_blank\" class=\"xst\" >)|(<a href=)", "");
+                                Matcher matcher = PATTERN.matcher(s);
+                                if (matcher.matches()) {
+                                    for (int j = 0; j < 6; ++j) {
+                                        if (lines[i + j].contains("悬赏") || lines[i + j].contains("金粒")) {
+                                            if (!list.contains(s)) {
+                                                if (!found) {
+                                                    System.out.println();
+                                                    found = true;
                                                 }
+                                                list.addLast(s);
+                                                while (list.size() > 70) {
+                                                    list.removeFirst();
+                                                }
+                                                if (Main.state) {
+                                                    try {
+                                                        Desktop.getDesktop().browse(new URI(urlPrefix + matcher.group("url")));
+                                                    } catch (IOException | URISyntaxException e) {
+                                                        e.printStackTrace();
+                                                    }
+                                                }
+                                                System.out.println(LocalTime.now() + " " + s.replace(matcher.group("url"), urlPrefix + matcher.group("url")));
                                             }
-                                            System.out.println(LocalDateTime.now() + " " + s.replace(matcher.group("url"), urlPrefix + matcher.group("url")));
+                                            break;
                                         }
-                                        break;
                                     }
                                 }
                             }
                         }
                     }
+                    Thread.sleep(2500);
                 }
-                Thread.sleep(2500);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("爬问答线程终止");
         }
-
     }
 }
